598570f7fe3fed74a212461e690372d79350eea2
Anil Shrestha <jesnilshrestha04@hotmail.com>
2015-11-27 18:48:39 -0800

    Enhanced Layout

--- server.js
+++ server.js
@@ -36,12 +36,6 @@ db.once('open', function callback(){
 	console.log('meanApp db opened');
 });
 
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-  mongoMessage = messageDoc.message;
-});
 
 app.get('/partials/:partialPath', function(req, res){
 	res.render('partials/' + req.params.partialPath);
@@ -50,9 +44,7 @@ app.get('/partials/:partialPath', function(req, res){
 
 
 app.get('*', function (req, res) {
-	res.render('index', {
-	mongoMessage: mongoMessage
-	});
+	res.render('index');
 });
 
 var port = process.env.PORT || 3030;
