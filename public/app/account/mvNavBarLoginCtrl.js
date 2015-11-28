95512d08596a3b99cb9d32b504ecdf268a4b76b2
Anil Shrestha <jesnilshrestha04@hotmail.com>
2015-11-28 00:22:52 -0800

    Angular Infinte Loop Fixed

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('mvNavBarLoginCtrl', function($scope){
-	$scope.signing = function (username, passwrod) {
+	$scope.signin = function (username, passwrod) {
 		console.log("I'm not done yet");
 	}
 });
